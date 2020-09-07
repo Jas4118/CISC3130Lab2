60d93ae85ccb2dfecd40b2c085d336777ae55cb5
Jas4118 <70446078+Jas4118@users.noreply.github.com>
2020-09-07 13:24:29 -0400

    Update Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -14,17 +14,17 @@ public class Lab2 {
 	public static void main(String[]args) throws IOException {
 		FileReader fr = new FileReader("regional-global-daily-latest.csv"); //Have to use BufferedReader because it stopped reading when I used Scanner
 	        BufferedReader br = new BufferedReader(fr);
-	        File output= new File(("strOutputF.txt")); 
+	        File output= new File(("Artists-WeekOf09062020")); //Prints to this file
 	        PrintWriter outputWriter = new PrintWriter(output);
-	        Set<artistName> names=new HashSet<artistName>();
+	        Set<artistName> names=new HashSet<artistName>(); //Creating a hashset to put all my data in
 	        br.readLine();
 	        br.readLine();
 	        HashSet<artistName> nameSet = new HashSet<artistName>(); //Creating a hashset to put all my data in
-	        for (String line = br.readLine(); line != null; line = br.readLine()) { //
-	        	String nameLine=line.split(",")[2]; // Sets the line with the artist name
+	        for (String line = br.readLine(); line != null; line = br.readLine()) { // Checks the file if theres a  line and sets the line to a string for nameLine
+	        	String nameLine=line.split(",")[2]; // Sets the line with the artist name through split
 	        	artistName temp=new artistName(nameLine);
 	        	if (nameSet.add(temp)==false) { // If the object wasn't added into the hashset
-	        		for(artistName artist : nameSet ) { //iterates by looking for the artist Obj
+	        		for(artistName artist : nameSet ) { //Goes through by looking for the artist Obj
 	        			if(artist.equals(temp)) {
 	        				artist.setFrequency(artist.getFrequency()+1);
 	        			}
